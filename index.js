4dc4f660b48f191262bfa425f744dcad2a70d1dc
martina <martinaferro000@gmail.com>
2021-05-24 18:33:06 +0200

    Committing all files for the dice rolling simulation site

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelector("button").onclick = function(){roll()}
+
+function roll() {
+
+    /***** First Dice *****/
+    var randomNumber1 = Math.floor(Math.random()*6)+1;
+    var src1 = "images/dice" + randomNumber1 + ".png"; 
+    var img1 = document.querySelector("#img1");
+    img1.setAttribute("src", src1);
+
+    /***** Second Dice *****/
+    var randomNumber2 = Math.floor(Math.random()*6)+1;
+    var src2 = "images/dice" + randomNumber2 + ".png"; 
+    var img2 = document.querySelector("#img2");
+    img2.setAttribute("src", src2);
+
+    /***** Message *****/
+    var heading = document.querySelector("h1");
+    if (randomNumber1>randomNumber2) {
+        heading.innerText = "🚩Player 1 Wins!";
+        heading.style.margin = "20px";
+    } else if (randomNumber1<randomNumber2) {
+        heading.innerText = "Player 2 Wins 🚩";
+        heading.style.margin = "20px";
+    } else {
+        heading.innerText = "Draw!"
+        heading.style.margin = "25px";
+    }
+
+}
